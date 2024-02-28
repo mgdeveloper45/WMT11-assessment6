e66d786fcdf9e8ab499bbfc75a2c6acd5bc16e75
MarkGee45 <moel55.mg@gmail.com>
2024-02-27 23:18:58 -0800

    added a third automation test

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -4,6 +4,7 @@ let driver;
 
 beforeEach(async () => {
   driver = await new Builder().forBrowser(Browser.CHROME).build();
+  await driver.get("http://localhost:8000");
 });
 
 afterEach(async () => {
@@ -12,15 +13,15 @@ afterEach(async () => {
 
 describe("Duel Duo tests", () => {
   test("page loads with title", async () => {
-    await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
   test("see all bots button get all bots", async () => {
-    await driver.get("http://localhost:8000");
     await driver.findElement(By.css('button')).click();
   });
   test("see that the Draw button displays the div with id = choices", async () => {
-    await driver.get("http://localhost:8000");
+    await driver.findElement(By.css('button')).click();
+  });
+  test("see that the remove from duo button removes the card with id = player-duo", async () => {
     await driver.findElement(By.css('button')).click();
   });
 });
